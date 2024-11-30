48826880d2363fb8c7feb94f6d13666b7f47e16d
rosebeats <sophie.d21401@gmail.com>
2024-11-30 16:24:53 -0600

    Write proper system prompt

--- mealie/services/cook_along/cook_along_service.py
+++ mealie/services/cook_along/cook_along_service.py
@@ -1,10 +1,16 @@
 import itertools
+import json
+import os
+from pathlib import Path
 
+from jinja2 import Environment, FileSystemLoader
 from openai.resources.chat.completions import ChatCompletionMessageParam
 
 from mealie.schema.recipe.recipe import Recipe
 from mealie.services._base_service import BaseService
 from mealie.services.cook_along import CookAlongMessagingService
+
+PROMPTS_DIR = Path(os.path.dirname(os.path.abspath(__file__))) / "prompts"
 
 
 class CookAlongService(BaseService):
@@ -18,7 +24,23 @@ class CookAlongService(BaseService):
         super().__init__()
 
     def build_prompt(self) -> ChatCompletionMessageParam:
-        return {"role": "system", "content": "The user will ask a question of you. Please just answer the question."}
+        env = Environment(loader=FileSystemLoader(PROMPTS_DIR))
+        template = env.get_template("system_prompt.jinja")
+        model_params = {
+            "description": self.recipe.description,
+            "ingredients": "\n".join([ingredient.display for ingredient in self.recipe.recipe_ingredient]),
+            "instructions": "\n\n".join([self.join_title_text(step) for step in self.recipe.recipe_instructions or []]),
+            "notes": "\n\n".join([self.join_title_text(note) for note in self.recipe.notes or []]),
+        }
+        system_prompt = template.render(model_params)
+        return {"role": "system", "content": json.dumps(system_prompt)}
+
+    @staticmethod
+    def join_title_text(element):
+        header = ""
+        if element.title is not None and element.title != "":
+            header = element.title + "\n"
+        return header + element.text
 
     async def ask_question(self, question: str) -> str | None:
         question_obj = {"role": "user", "content": question}
